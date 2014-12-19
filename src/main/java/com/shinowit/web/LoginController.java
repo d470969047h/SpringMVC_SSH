323391ae692383cf2cf1c1acf0f12ab664c41ae8
d470969047h <d470969047h@gmail.com>
2014-12-19 10:25:05 +0800

    @RequestBody注解绑定请求参数内容到方法参数中，请求参数内容可以是json数据、字节数组等

--- src/main/java/com/shinowit/web/LoginController.java
+++ src/main/java/com/shinowit/web/LoginController.java
@@ -1,19 +1,27 @@
 package com.shinowit.web;
 
 import com.shinowit.entity.UserEntity;
+import com.shinowit.framework.dao.BaseDAO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.annotation.Resource;
+
 /**
  * Created by daihui on 2014-12-18.
+ * *@RequestBody注解绑定请求参数内容到方法参数中，请求参数内容可以是json数据、字节数组等
  */
 @Controller
 @RequestMapping("/login")
 public class LoginController {
+    @Resource
+    private BaseDAO<UserEntity> userEntityDAO;
+
     @RequestMapping("/login")
     public  @ResponseBody UserEntity login(@RequestBody UserEntity userEntity) {
-        return userEntity;
+       userEntityDAO.insert(userEntity);
+            return userEntity;
     }
 }
